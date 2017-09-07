2bc9b88f9670d616ca527b8e00651ac88ba514c2
Luiz Fernando A. Vasconcellos <lfav17@inf.ufpr.br>
2017-09-07 15:47:09 -0300

    Removes libraries not used

--- periodo-1.c
+++ periodo-1.c
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "lcrandom.h"
 
 unsigned long period(unsigned long, unsigned long,
--- periodo-2.c
+++ periodo-2.c
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "lcrandom.h"
 
 #define A0 1103515245
